66175445f72070d46a72eae4279183317cf9891f
Moshanov Abylai <abylaikhhann@mail.ru>
2025-08-25 00:12:04 +0500

    fix bugs

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse, reverse_lazy
 from django.utils import timezone as tz
 from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+from django.http import HttpResponseForbidden
 
 # Локальные импорты
 from blog.models import Category, Comment, Post
@@ -73,6 +74,37 @@ class OnlyAuthorMixin(UserPassesTestMixin):
     def test_func(self):
         obj = self.get_object()
         return obj.author == self.request.user
+    
+
+class LoginRequiredAndPostAuthorMixin(UserPassesTestMixin):
+    
+    def test_func(self):
+        if not self.request.user.is_authenticated:
+            return False
+        obj = self.get_object()
+        return obj.author == self.request.user
+    
+    def handle_no_permission(self):
+        if not self.request.user.is_authenticated:
+            post_id = self.kwargs.get('post_id')
+            return redirect('blog:post_detail', post_id=post_id)
+        return HttpResponseForbidden("У вас нет прав для этого действия")
+
+
+class LoginRequiredAndCommentAuthorMixin(UserPassesTestMixin):
+    
+    def test_func(self):
+        if not self.request.user.is_authenticated:
+            return False
+        obj = self.get_object()
+        return obj.author == self.request.user
+    
+    def handle_no_permission(self):
+        if not self.request.user.is_authenticated:
+            comment_id = self.kwargs.get('comment_id')
+            comment = get_object_or_404(Comment, pk=comment_id)
+            return redirect('blog:post_detail', post_id=comment.post_id)
+        return HttpResponseForbidden("У вас нет прав для этого действия")
 
 
 class PostMixin():
@@ -82,7 +114,7 @@ class PostMixin():
     exclude = ('author',)
 
 
-class PostCreateView(LoginRequiredMixin,PostMixin, CreateView):
+class PostCreateView(LoginRequiredMixin, PostMixin, CreateView):
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -92,11 +124,11 @@ class PostCreateView(LoginRequiredMixin,PostMixin, CreateView):
         return reverse('blog:profile', kwargs={'username': self.request.user.username})
 
 
-class PostUpdateView(OnlyAuthorMixin, PostMixin, UpdateView):
+class PostUpdateView(LoginRequiredAndPostAuthorMixin, PostMixin, UpdateView):
     pk_url_kwarg = 'post_id'
 
 
-class PostDeleteView(OnlyAuthorMixin, DeleteView):
+class PostDeleteView(LoginRequiredAndPostAuthorMixin, DeleteView):
     model = Post
     pk_url_kwarg = 'post_id'
     success_url = reverse_lazy('blog:index')
@@ -167,14 +199,14 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
         return reverse('blog:post_detail', kwargs={'post_id': self.kwargs['post_id']})
 
 
-class CommentUpdateView(UpdateView):
+class CommentUpdateView(LoginRequiredAndCommentAuthorMixin, UpdateView):
     model = Comment
     form_class = CommentForm
     pk_url_kwarg = 'comment_id'
     template_name = 'blog/comment.html'
 
 
-class CommentDeleteView(DeleteView):
+class CommentDeleteView(LoginRequiredAndCommentAuthorMixin, DeleteView):
     model = Comment
     pk_url_kwarg = 'comment_id'
     template_name = 'blog/comment.html'
