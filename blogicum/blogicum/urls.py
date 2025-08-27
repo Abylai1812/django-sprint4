186f6c077d3e899885f1a20289ea14da9bf67370
Moshanov Abylai <abylaikhhann@mail.ru>
2025-08-27 06:03:19 +0500

    fix bugs

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -1,14 +1,12 @@
-from django.contrib import admin
-from django.urls import include, path, reverse_lazy
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib import admin
 from django.contrib.auth.forms import UserCreationForm
+from django.urls import include, path, reverse_lazy
 from django.views.generic.edit import CreateView
 
 
 urlpatterns = [
-    path('', include('blog.urls')),
     path('pages/', include('pages.urls')),
     path('admin/', admin.site.urls),
     path('auth/', include('django.contrib.auth.urls')),
@@ -22,12 +20,16 @@ urlpatterns = [
         ),
         name='registration',
     ),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', include('blog.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    import debug_toolbar
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
+
 
 handler404 = 'pages.views.page_not_found'
 handler403 = 'pages.views.csrf_failure'
 handler500 = 'pages.views.server_error'
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
